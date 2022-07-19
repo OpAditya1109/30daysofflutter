60ea704439f902755e0ab3acc763715593e8344a
Aditya <adityay1109@gmail.com>
2022-07-19 20:54:12 +0530

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catlog/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    int days = 40;
+    String name = "Codepur";
+    //double pi = 3.14;
+    //bool isMale = true;
+    //num temp = 30.5;
+
+    //var day = "Tuesday";
+    //const pi = 3.14;
+    //final 
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days of flutter"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
